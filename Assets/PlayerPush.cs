efa038577c3c7f1f40bd08c32f211f720dfc134c
Raimon <ramonkoudsi@gmail.com>
2020-02-01 11:17:50 -0400

    error

--- Assets/PlayerPush.cs
+++ Assets/PlayerPush.cs
@@ -22,9 +22,15 @@ public class PlayerPush : MonoBehaviour
         Physics2D.queriesStartInColliders=false;
         RaycastHit2D hit=Physics2D.Raycast(transform.position, Vector2.right*transform.localScale.x, distance, boxMask);
 
-        if(hit.collider != null && Input.GetKey(KeyCode.E))
+        if(hit.collider != null && hit.collider.gameObject.tag=="pushable" && Input.GetKey(KeyCode.E))
         {
             box = hit.collider.gameObject;
+
+            box.GetComponent<FixedJoint2D>().enabled=true;
+            box.GetComponent<FixedJoint2D>().connectedBody=this.GetComponent<Rigidbody2D>();
+        } else if(Input.GetKeyUp (KeyCode.E))
+        {
+            box.GetComponent<FixedJoint2D>().enabled=false;
         }
     }
 
