cfac911593407c7057cc14ed234b2c4260a37ef2
Raimon <ramonkoudsi@gmail.com>
2020-02-01 11:52:48 -0400

    Auto stash before merge of "master" and "origin/master"

--- Assets/PlayerPush.cs
+++ Assets/PlayerPush.cs
@@ -22,16 +22,18 @@ public class PlayerPush : MonoBehaviour
         Physics2D.queriesStartInColliders=false;
         RaycastHit2D hit=Physics2D.Raycast(transform.position, Vector2.right*transform.localScale.x, distance, boxMask);
 
-        if(hit.collider != null && hit.collider.gameObject.tag=="pushable" && Input.GetKey(KeyCode.E))
+        if(hit.collider != null && hit.collider.gameObject.tag=="Pushable" && Input.GetKey(KeyCode.E))
         {
             box = hit.collider.gameObject;
 
             box.GetComponent<FixedJoint2D>().enabled=true;
             box.GetComponent<FixedJoint2D>().connectedBody=this.GetComponent<Rigidbody2D>();
-        } else if(Input.GetKeyUp (KeyCode.E))
+        } 
+        else if(Input.GetKeyUp (KeyCode.E))
         {
             box.GetComponent<FixedJoint2D>().enabled=false;
         }
+        
     }
 
 
