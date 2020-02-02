5512e74cec8f7fc74ef54812057db56114c92ee8
Raimon <ramonkoudsi@gmail.com>
2020-02-02 09:51:42 -0400

    cambios en main

--- Assets/Scripts/Hazy/puzzle.cs
+++ Assets/Scripts/Hazy/puzzle.cs
@@ -4,9 +4,16 @@ using UnityEngine;
 
 public class puzzle : MonoBehaviour
 {
-    public GameObject palanca1, palanca2, palanca3, palanca4, palanca5;
+    public GameObject palanca1, palanca2, palanca3, palanca4, palanca5, puerta, target, player;
+
+    
+    private Player scriptPlayer;
 
     private trigger trigger1,trigger2,trigger3,trigger4,trigger5; 
+
+    private bool activarPuerta;
+
+    public float velocidad;
 
 
     // Start is called before the first frame update
@@ -17,6 +24,8 @@ public class puzzle : MonoBehaviour
         trigger3=palanca3.GetComponent<trigger>();
         trigger4=palanca4.GetComponent<trigger>();
         trigger5=palanca5.GetComponent<trigger>();
+
+        scriptPlayer = player.GetComponent<Player>();
     }
 
     // Update is called once per frame
@@ -55,6 +64,33 @@ public class puzzle : MonoBehaviour
             trigger4.palanca=!trigger4.palanca;
 
             trigger5.presionado=false;
+        }
+
+
+        if(trigger1.palanca && trigger2.palanca && trigger3.palanca && trigger4.palanca && trigger5.palanca)
+        {
+            trigger1.enabled=false;
+            trigger2.enabled=false;
+            trigger3.enabled=false;
+            trigger4.enabled=false;
+            trigger5.enabled=false;
+
+            scriptPlayer.enabled = false;
+            scriptPlayer.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+
+            float velocidad_nueva = velocidad * Time.deltaTime;
+            puerta.transform.position = Vector3.MoveTowards(puerta.transform.position, target.transform.position, velocidad_nueva);
+
+            if(puerta.transform.position == target.transform.position)
+            {
+                trigger1.palanca=false;
+                trigger2.palanca=false;
+                trigger3.palanca=false;
+                trigger4.palanca=false;
+                trigger5.palanca=false;
+
+                scriptPlayer.enabled = true;
+            }
         }
         
     }
--- Assets/Scripts/trigger.cs
+++ Assets/Scripts/trigger.cs
@@ -25,6 +25,7 @@ private void Update() {
 
         presionado=true;
     }
+    
 
     if(palanca==true)
     {
