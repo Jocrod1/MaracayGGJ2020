43f791615d9d3473b2e107db7e30ec6ba358fcb6
Jose Pereira <wallbangstudios4.9@gmail.com>
2020-02-02 03:12:27 -0400

    menu prinicipal y reorganizacion

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+
+public class MenuScript : MonoBehaviour {
+
+    public AudioMixer AM;
+
+    public AudioSource SelectSound;
+
+
+    public void playgame(string Level) {
+        SceneManager.LoadScene(Level);
+    }
+    public void Quit() {
+        Application.Quit();
+    }
+}
